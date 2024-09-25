0cfd0cee77b6cee2dbfced8517be6661a4a784c6
arthurnxtto <arthurvnetto8@outlook.com>
2024-09-24 22:20:24 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/nutriapp/EstoqueFragment.java
@@ -0,0 +1,73 @@
+package br.com.etecia.nutriapp;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class EstoqueFragment extends Fragment {
+
+    RecyclerView idRecViewEstoque;
+    List<Produto> produtoList;
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_estoque, container, false);
+
+        idRecViewEstoque = view.findViewById(R.id.idRecEstoque);
+
+        produtoList = new ArrayList<>();
+        produtoList.add(new Produto("Arroz",  50, 20.50));
+        produtoList.add(new Produto("Feijão", 30, 40.50));
+        produtoList.add(new Produto("Batata", 25, 30.40));
+
+        EstoqueAdapter estoqueAdapter = new EstoqueAdapter(getContext(), produtoList);
+        idRecViewEstoque.setLayoutManager(new GridLayoutManager(getContext(), 1));
+        idRecViewEstoque.setHasFixedSize(true);
+
+        idRecViewEstoque.setAdapter(estoqueAdapter);
+
+        estoqueAdapter = new EstoqueAdapter(getContext(), produtoList);
+
+
+        idRecViewEstoque.setLayoutManager(new
+                GridLayoutManager(
+                getContext(), 1));
+
+        //fixador de tamanho da lista - deixar a lista mais rápida
+        idRecViewEstoque.setHasFixedSize(true);
+
+        //ligar o recyclerview ao adaptador
+        idRecViewEstoque.setAdapter(estoqueAdapter);
+
+
+
+
+
+        return view;
+
+
+
+    }
+
+
+
+
+}
